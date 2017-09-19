5601f51f01ea377ac11c19970ef3ab0d50af03de
D7702_10 <d7702_10@d7702-10ui-imac.local>
2017-09-19 15:50:50 +0900

    Plist

--- 03_Map_Pin_Plist/ViewController.swift
+++ 03_Map_Pin_Plist/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myMap: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+            zoomToRegion()
+        
+            // plist 파일을 경로 찾아서 가져 오기
+        let path = Bundle.main.path(forResource: "ViewPoint", ofType: "plist")
+        print("path = \(String(describing:path))")
+    
+            // plist 파일 내용을 가져와서 저장하기.
+        let content = NSArray(contentsOfFile: path!)
+        print("contents = \(String(describing: content))")
+        
+        //pin저장
+        var annotations = [MKPointAnnotation]()
+        
+        if let myitems = content {
+            for item in myitems{
+                let lat = (item as AnyObject).value(forKey: "lat")
+                let long = (item as AnyObject).value(forKey: "long")
+                let title = (item as AnyObject).value(forKey: "title")
+                let subtitle = (item as AnyObject).value(forKey: "subTitle")
+                let annotation = MKPointAnnotation()
+                
+               // 형 변환
+                let myLat = (lat as! NSString).doubleValue
+                let myLong = (long as! NSString).doubleValue
+                let mytitle = title as! String
+                let mysubtitle = subtitle as! String
+                
+                annotation.coordinate.latitude = myLat
+                annotation.coordinate.longitude = myLong
+                annotation.title = mytitle
+                annotation.subtitle = mysubtitle
+            
+                //배열에 추가하기
+                
+                annotations.append(annotation)
+                
+                
+                
+                
+            }
+            
+        }   else{
+                print("content is nil")
+            }
+        
+                myMap.addAnnotations(annotations)
+                myMap.showAnnotations(annotations, animated: true)
+        
+        }
+    
+        func zoomToRegion(){
+        let center = CLLocationCoordinate2DMake(35.166197, 129.072594)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        myMap.setRegion(region, animated: true)
+ 
+            
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
